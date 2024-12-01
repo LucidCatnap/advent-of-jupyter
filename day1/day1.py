53e002473e5791970051771ec86a5c1a53082693
Fredrick Blikeng <lucidcatnap@gmail.com>
2024-12-01 21:30:42 +0100

    Setup

---
+++ day1/day1.py
@@ -0,0 +1,21 @@
+# ---
+# jupyter:
+#   jupytext:
+#     custom_cell_magics: kql
+#     text_representation:
+#       extension: .py
+#       format_name: percent
+#       format_version: '1.3'
+#       jupytext_version: 1.11.2
+#   kernelspec:
+#     display_name: .venv
+#     language: python
+#     name: python3
+# ---
+
+# %% [markdown]
+# ## Day 1
+#
+# Check the link in seven hours.
+#
+# https://adventofcode.com/
